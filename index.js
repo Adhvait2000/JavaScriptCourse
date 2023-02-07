d26ebfcc1b40c1cce71550b5beb4c0d115ebf17e
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-02-08 00:52:15 +0800

    Update

---
+++ index.js
@@ -0,0 +1,9 @@
+//document.getElementById("count-el").innerText = 5
+
+//creating first variables of JavaScript
+
+//need variables to store data in 
+
+let count = 0
+
+console.log(count) //print function in JavaScript
